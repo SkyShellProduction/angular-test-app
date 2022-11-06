b515fb3ff5cd111f97e4a93e1e6517d0d9d102e0
vova <drozdov_vova2110@mail.ru>
2022-11-06 21:22:16 +0500

    my first angular app, not fineished yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+// import {products} from './data/products';
+import { Observable, tap } from 'rxjs'
+import { ProductService } from './services/products.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test-app';
+export class AppComponent implements OnInit{
+  title = 'Angular Products';
+  // products: IProduct[] = [];
+  products$: Observable<IProduct[]>
+  loading = false;
+  constructor(private productsService: ProductService){
+
+  }
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products;
+    //   this.loading = false;
+    // })
+  }
+
 }
